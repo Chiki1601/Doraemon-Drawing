c69403ee04e66dee25e8835cba6dcd48d6824d45
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-02-12 11:11:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from turtle import *
 
 
 # Doraemon with Python Turtle
-def ankur(x, y):
+def pooja(x, y):
     penup()
     goto(x, y)
     pendown()
@@ -30,33 +30,33 @@ def aankha():
 
 
 def daari():
-    ankur(-32, 135)
+    pooja(-32, 135)
     seth(165)
     fd(60)
 
-    ankur(-32, 125)
+    pooja(-32, 125)
     seth(180)
     fd(60)
 
-    ankur(-32, 115)
+    pooja(-32, 115)
     seth(193)
     fd(60)
 
-    ankur(37, 135)
+    pooja(37, 135)
     seth(15)
     fd(60)
 
-    ankur(37, 125)
+    pooja(37, 125)
     seth(0)
     fd(60)
 
-    ankur(37, 115)
+    pooja(37, 115)
     seth(-13)
     fd(60)
 
 
 def mukh():
-    ankur(5, 148)
+    pooja(5, 148)
     seth(270)
     fd(100)
     seth(0)
@@ -81,7 +81,7 @@ def muflar():
 
 
 def nak():
-    ankur(-10, 158)
+    pooja(-10, 158)
     seth(315)
     fillcolor('#e70010')
     begin_fill()
@@ -91,25 +91,25 @@ def nak():
 
 def black_aankha():
     seth(0)
-    ankur(-20, 195)
+    pooja(-20, 195)
     fillcolor('#000000')
     begin_fill()
     circle(13)
     end_fill()
 
     pensize(6)
-    ankur(20, 205)
+    pooja(20, 205)
     seth(75)
     circle(-10, 150)
     pensize(3)
 
-    ankur(-17, 200)
+    pooja(-17, 200)
     seth(0)
     fillcolor('#ffffff')
     begin_fill()
     circle(5)
     end_fill()
-    ankur(0, 0)
+    pooja(0, 0)
 
 
 def face():
@@ -125,7 +125,7 @@ def face():
     seth(215)
     circle(120, 100)
     end_fill()
-    ankur(63.56, 218.24)
+    pooja(63.56, 218.24)
     seth(90)
     aankha()
     seth(180)
@@ -162,7 +162,7 @@ def Doraemon():
 
     daari()
 
-    ankur(0, 0)
+    pooja(0, 0)
 
     seth(0)
     penup()
@@ -224,7 +224,7 @@ def Doraemon():
     circle(-30)
     end_fill()
 
-    ankur(103.74, -182.59)
+    pooja(103.74, -182.59)
     seth(0)
     fillcolor('#ffffff')
     begin_fill()
@@ -235,7 +235,7 @@ def Doraemon():
     fd(10)
     end_fill()
 
-    ankur(-96.26, -182.59)
+    pooja(-96.26, -182.59)
     seth(180)
     fillcolor('#ffffff')
     begin_fill()
@@ -246,7 +246,7 @@ def Doraemon():
     fd(10)
     end_fill()
 
-    ankur(-133.97, -91.81)
+    pooja(-133.97, -91.81)
     seth(50)
     fillcolor('#ffffff')
     begin_fill()
@@ -254,7 +254,7 @@ def Doraemon():
     end_fill()
     # Doraemon with Python Turtle
 
-    ankur(-103.42, 15.09)
+    pooja(-103.42, 15.09)
     seth(0)
     fd(38)
     seth(230)
@@ -262,7 +262,7 @@ def Doraemon():
     circle(90, 260)
     end_fill()
 
-    ankur(5, -40)
+    pooja(5, -40)
     seth(0)
     fd(70)
     seth(-90)
@@ -270,7 +270,7 @@ def Doraemon():
     seth(0)
     fd(70)
 
-    ankur(-103.42, 15.09)
+    pooja(-103.42, 15.09)
     fd(90)
     seth(70)
     fillcolor('#ffd200')
@@ -294,7 +294,7 @@ def Doraemon():
     seth(90)
     fd(15)
     dot(10)
-    ankur(0, -150)
+    pooja(0, -150)
 
     black_aankha()
 if __name__ == '__main__':
@@ -302,6 +302,6 @@ if __name__ == '__main__':
     pensize(3)
     speed(9)
     Doraemon()
-    ankur(100, -300)
+    pooja(100, -300)
     write('by Pooja Patel', font=("Bradley Hand ITC", 30, "bold"))
     mainloop()
